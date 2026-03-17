72878744193f584b9e2cddeb50de91cb2ece84c1
Wagner Alves <wagneralvesviana99@gmail.com>
2026-03-17 17:30:53 -0300

    configurando o pages github via vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/FlixReact/',
+   build: {
+    outDir: 'build'   // 👈 gera a pasta "build" em vez de "dist"
+  }
 })
